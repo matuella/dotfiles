9dd0b9346595a72b85cef3917329c83f6eef5ae8
matuella <guilherme1matuella@gmail.com>
2024-07-09 12:49:49 -0300

    Add missing signing key to github SSH setup

--- config_sensitive.sh
+++ config_sensitive.sh
@@ -2,5 +2,5 @@ export DF_GIT_USERNAME=""
 export DF_GIT_EMAIL=""
 
 export DF_SSH_KEY_EMAIL=""
-export DF_SSH_KEY_FILE=""
+export DF_SSH_KEY_PREFIX=""
 export DF_SSH_PASSPHRASE=""
--- setup/github-ssh.sh
+++ setup/github-ssh.sh
@@ -5,18 +5,22 @@
 ###
 
 script_info "[github-ssh] Generating SSH key pair..."
-if [ -z "$DF_SSH_KEY_FILE" ] || [ -z "$DF_SSH_KEY_EMAIL" ] || [ -z "$DF_SSH_PASSPHRASE" ]; then
+if [ -z "$DF_SSH_KEY_PREFIX" ] || [ -z "$DF_SSH_KEY_EMAIL" ] || [ -z "$DF_SSH_PASSPHRASE" ]; then
     script_info "[github-ssh] SKIPPED: 'DF_SSH_*' properties not set on config files..."
     exit 0
 fi
 
-if test -f "$HOME/.ssh/$DF_SSH_KEY_FILE"; then
-    script_info "[github-ssh] SKIPPED: github ssh key already exists..."
+auth_key_file="auth_${DF_SSH_KEY_PREFIX}_id_ed25519"
+signing_key_file="signing_${DF_SSH_KEY_PREFIX}_id_ed25519"
+
+if test -f "$HOME/.ssh/$auth_key_file" || test -f "$HOME/.ssh/$signing_key_file"; then
+    script_info "[github-ssh] SKIPPED: at least one github ssh key already exists..."
     exit 0
 fi
 
 eval_brew
-ssh-keygen -t ed25519 -N "$DF_SSH_PASSPHRASE" -f ~/.ssh/$DF_SSH_KEY_FILE -C "$DF_SSH_KEY_EMAIL"
+ssh-keygen -t ed25519 -N "$DF_SSH_PASSPHRASE" -f ~/.ssh/$auth_key_file -C "$DF_SSH_KEY_EMAIL"
+ssh-keygen -t ed25519 -N "$DF_SSH_PASSPHRASE" -f ~/.ssh/$signing_key_file -C "$DF_SSH_KEY_EMAIL"
 script_info "[github-ssh] Initializing ssh-agent..."
 eval "$(ssh-agent -s)"
 
@@ -30,28 +34,34 @@ echo "
 Host github.com
   AddKeysToAgent yes
   UseKeychain yes
-  IdentityFile ~/.ssh/$DF_SSH_KEY_FILE" >> ~/.ssh/config
+  IdentityFile ~/.ssh/$auth_key_file" >> ~/.ssh/config
 
 script_info "[github-ssh] Adding (ssh-add) keychain usage to key..."
 
 # Workaround to not ask for passphrase: https://stackoverflow.com/a/73624704
 tmp_askpass=$(mktemp)
-printf '#!/bin/sh\necho "$SSH_PASSPHRASE"\n' >> $tmp_askpass
+printf '#!/bin/sh\necho "$DF_SSH_PASSPHRASE"\n' >> $tmp_askpass
 chmod +x $tmp_askpass
-SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$tmp_askpass" ssh-add --apple-use-keychain ~/.ssh/$DF_SSH_KEY_FILE
+SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$tmp_askpass" ssh-add --apple-use-keychain ~/.ssh/$auth_key_file
+SSH_ASKPASS_REQUIRE=force SSH_ASKPASS="$tmp_askpass" ssh-add --apple-use-keychain ~/.ssh/$signing_key_file
 rm -f "$tmp_askpass"
 
 # Add gh to known hosts to prevent asking passphrase on first clone using SSH
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-# Configure git to use the SSH key to sign commits globally
+# Configure git to use the proper SSH key to sign commits globally
 git config --global commit.gpgsign true
 git config --global gpg.format ssh
-git config --global user.signingkey ~/.ssh/$DF_SSH_KEY_FILE.pub
+git config --global user.signingkey ~/.ssh/$signing_key_file.pub
 
-pbcopy < ~/.ssh/$DF_SSH_KEY_FILE.pub
-prompt_important "[github-ssh] MANUAL OPERATION REQUIRED: Your SSH is setup and the public key is copied to your clipboard!
-Press any key to open the GitHub SSH settings page and paste the key there, with a proper title."
-
+prompt_important "[github-ssh] MANUAL OPERATION REQUIRED: Your AUTHENTICATION SSH is setup!
+Press any key to copy the public key to your clipboard and open the GitHub SSH screen. Create a key there of type Authentication!"
+pbcopy < ~/.ssh/$auth_key_file.pub
 open "https://github.com/settings/ssh/new"
+
+prompt_important "[github-ssh] MANUAL OPERATION REQUIRED: Your SIGNING SSH is setup!
+Press any key to copy the public key to your clipboard and open the GitHub SSH screen. Create a key there of type Signing!"
+pbcopy < ~/.ssh/$signing_key_file.pub
+open "https://github.com/settings/ssh/new"
+
 prompt_important "[github-ssh] MANUAL OPERATION REQUIRED: Once you've created it, press any key to continue."
